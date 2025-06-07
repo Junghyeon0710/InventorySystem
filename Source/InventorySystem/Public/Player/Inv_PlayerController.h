f149855b0c19a4cb95ceda56fb7bd5699901be10
Junghyeon0710 <sie08357@naver.com>
2025-06-07 18:25:27 +0900

    Inventory Player Controller Add

---
+++ Source/InventorySystem/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual void BeginPlay() override;
+};
+
