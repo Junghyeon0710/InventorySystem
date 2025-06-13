cd63439ef3b4e6077bfab247298fd7b5bb7ce591
Junghyeon0710 <sie08357@naver.com>
2025-06-13 11:27:14 +0900

    21. Fast Array Serializer

---
+++ Source/InventorySystem/Private/InventoryManagement/FastArray/Inv_FastArray.cpp
@@ -0,0 +1,25 @@
+﻿#include "InventoryManagement/FastArray/Inv_FastArray.h"
+
+TArray<UInv_InventoryItem*> FInv_InventoryFastArray::GetItems() const
+{
+}
+
+void FInv_InventoryFastArray::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize)
+{
+}
+
+void FInv_InventoryFastArray::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
+{
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryComponent* ItemComponent)
+{
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryItem* Item)
+{
+}
+
+void FInv_InventoryFastArray::RemoveEntry(UInv_InventoryItem* Item)
+{
+}
---
+++ Source/InventorySystem/Public/InventoryManagement/FastArray/Inv_FastArray.h
@@ -0,0 +1,67 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Net/Serialization/FastArraySerializer.h"
+
+#include "Inv_FastArray.generated.h"
+
+class UInv_InventoryComponent;
+class UInv_InventoryItem;
+
+/** 인벤토리 안의 단일 항목 */
+USTRUCT(BlueprintType)
+struct FInv_InventoryEntry : public FFastArraySerializerItem
+{
+	GENERATED_BODY()
+
+	FInv_InventoryEntry() {}
+
+private:
+	friend struct FInv_InventoryFastArray;
+	friend UInv_InventoryComponent;
+
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryItem> Item = nullptr;
+};
+
+/** 인벤토리 아이템들의 리스트 */
+USTRUCT(BlueprintType)
+struct FInv_InventoryFastArray : public FFastArraySerializer
+{
+	GENERATED_BODY()
+public:
+	FInv_InventoryFastArray() : OwnerComponent(nullptr) {}
+	FInv_InventoryFastArray(UActorComponent* InOwnerComponent) : OwnerComponent(InOwnerComponent) {}
+
+	TArray<UInv_InventoryItem*> GetItems() const;
+
+	//~FFastArraySerializer contract
+	void PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize);
+	void PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize);
+	//void PostReplicatedChange(const TArrayView<int32> ChangedIndices, int32 FinalSize);
+	//~End of FFastArraySerializer contract
+
+	bool NetDeltaSerialize(FNetDeltaSerializeInfo& DeltaParms)
+	{
+		return FastArrayDeltaSerialize<FInv_InventoryEntry,FInv_InventoryFastArray>(Entries,DeltaParms,*this);
+	}
+
+	UInv_InventoryItem* AddEntry(UInv_InventoryComponent* ItemComponent);
+	UInv_InventoryItem* AddEntry(UInv_InventoryItem* Item);
+	void RemoveEntry(UInv_InventoryItem* Item);
+
+private:
+	friend UInv_InventoryComponent;
+
+	UPROPERTY(NotReplicated)
+	TArray<FInv_InventoryEntry> Entries;
+	
+	UPROPERTY(NotReplicated)
+	TObjectPtr<UActorComponent> OwnerComponent;
+};
+
+template<>
+struct TStructOpsTypeTraits<FInv_InventoryFastArray> : public  TStructOpsTypeTraitsBase2<FInv_InventoryFastArray>
+{
+	enum { WithNetDeltaSerializer = true  // 이 타입은 델타 직렬화를 지원한다};
+};
