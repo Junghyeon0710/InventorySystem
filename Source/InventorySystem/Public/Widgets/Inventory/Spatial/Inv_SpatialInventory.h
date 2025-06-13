3f24bfc2d2ff9faa83790105c42ea675b24172c7
Junghyeon0710 <sie08357@naver.com>
2025-06-13 15:41:02 +0900

    24. Has Room for Item

--- Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
+++ Source/InventorySystem/Public/Widgets/Inventory/Spatial/Inv_SpatialInventory.h
@@ -20,7 +20,8 @@ class INVENTORYSYSTEM_API UInv_SpatialInventory : public UInv_InventoryBase
 
 public:
 	virtual void NativeOnInitialized() override;
-
+	
+	virtual FInv_SlotAvailabilityResult HasRoomForItem(UInv_ItemComponent* ItemComponent) const override;
 private:
 	
 	UPROPERTY(meta  = (BindWidget))
