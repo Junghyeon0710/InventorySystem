9f9dcced98b0c7db281df610dc94e111aa9547a0
Junghyeon0710 <sie08357@naver.com>
2025-06-13 17:45:44 +0900

    27. Item Type Tags

--- Source/InventorySystem/Public/Items/Manifest/Inv_ItemManifest.h
+++ Source/InventorySystem/Public/Items/Manifest/Inv_ItemManifest.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Types/Inv_GridTypes.h"
 #include "StructUtils/InstancedStruct.h"
 
@@ -21,9 +22,13 @@ struct INVENTORYSYSTEM_API FInv_ItemManifest
 
 	UInv_InventoryItem* Manifest(UObject* NewOuter);
 	EInv_ItemCategory GetItemCategory() const {return ItemCategory;}
+	FGameplayTag GetItemType() const {return ItemType;}
 
 private:
 
 	UPROPERTY(EditAnywhere, Category="Inventory")
 	EInv_ItemCategory ItemCategory{EInv_ItemCategory::None};
+
+	UPROPERTY(EditAnywhere, Category="Inventory")
+	FGameplayTag ItemType;
 };
