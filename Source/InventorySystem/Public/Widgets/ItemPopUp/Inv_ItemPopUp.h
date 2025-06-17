227865cf6c10ec952f7f31211cf047ff76cde809
Junghyeon0710 <sie08357@naver.com>
2025-06-17 22:40:37 +0900

    97. Pop Up Menu Class

---
+++ Source/InventorySystem/Public/Widgets/ItemPopUp/Inv_ItemPopUp.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Inv_ItemPopUp.generated.h"
+
+class USizeBox;
+class UTextBlock;
+class UButton;
+class USlider;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_ItemPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<UButton> Button_Split;
+	
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<UButton> Button_Drop;
+
+	
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<UButton> Button_Consume;
+
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<USlider> Slider_Split;
+
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<UTextBlock> Slider_SplitAmount;
+
+	UPROPERTY(meta  = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox_Root;
+};
