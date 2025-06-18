a772e65ee549388986e227ccd45ddb65efbccd50
Junghyeon0710 <sie08357@naver.com>
2025-06-18 14:28:51 +0900

    116. Consumable Fragments

--- Source/InventorySystem/Private/Items/Fragments/Inv_ItemFragment.cpp
+++ Source/InventorySystem/Private/Items/Fragments/Inv_ItemFragment.cpp
@@ -1,1 +1,11 @@
 ﻿#include "Items/Fragments/Inv_ItemFragment.h"
+
+void FInv_HealthPotionFragment::OnConsume(APlayerController* PC)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, "Health Potion Consumed");
+}
+
+void FInv_ManaPotionFragment::OnConsume(APlayerController* PC)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Blue, "Mana Potion Consumed");
+}
