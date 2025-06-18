4543d83017a2e27a8323b1ce6f2a66b3a63b1e6b
Junghyeon0710 <sie08357@naver.com>
2025-06-18 14:51:53 +0900

    118. Item Description Widget

---
+++ Source/InventorySystem/Public/Widgets/ItemDescription/UInv_ItemDescription.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "UInv_ItemDescription.generated.h"
+
+class USizeBox;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UInv_ItemDescription : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+
+		FVector2D GetSize() const;
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USizeBox> SizeBox;
+};
