0de6698fd994d73a92e5b64a1753d7840f035c34
Junghyeon0710 <sie08357@naver.com>
2025-06-20 17:12:18 +0900

    152. Equipment Component

---
+++ Source/InventorySystem/Private/EquipmentManagement/Components/Inv_EquipmentComponent.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentManagement/Components/Inv_EquipmentComponent.h"
+
+#include "GameFramework/Character.h"
+#include "GameFramework/PlayerController.h"
+#include "InventoryManagement/Utils/Inv_InventoryStatics.h"
+#include "InventoryManagement/Components/Inv_InventoryComponent.h"
+
+void UInv_EquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OwningPlayerController = Cast<APlayerController>(GetOwner());
+	if (OwningPlayerController.IsValid())
+	{
+		ACharacter* OwnerCharacter = Cast<ACharacter>(OwningPlayerController->GetPawn());
+		if (IsValid(OwnerCharacter))
+		{
+			OwningSkeletalMesh = OwnerCharacter->GetMesh();
+		}
+		InitInventoryComponent();
+	}
+}
+
+void UInv_EquipmentComponent::InitInventoryComponent()
+{
+	InventoryComponent = UInv_InventoryStatics::GetInventoryComponent(OwningPlayerController.Get());
+	if (!InventoryComponent.IsValid())
+	{
+		return;
+	}
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this, &ThisClass::OnItemEquipped))
+	{
+		InventoryComponent->OnItemEquipped.AddDynamic(this, &ThisClass::OnItemEquipped);
+	}
+
+	if (!InventoryComponent->OnItemEquipped.IsAlreadyBound(this, &ThisClass::OnItemEquipped))
+	{
+		InventoryComponent->OnItemUnEquipped.AddDynamic(this, &ThisClass::OnItemUnEquipped);
+	}
+}
+
+void UInv_EquipmentComponent::OnItemEquipped(UInv_InventoryItem* EquippedItem)
+{
+}
+
+void UInv_EquipmentComponent::OnItemUnEquipped(UInv_InventoryItem* EquippedItem)
+{
+}
+
